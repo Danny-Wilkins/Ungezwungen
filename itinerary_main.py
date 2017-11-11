407387e52d8798fd56f1def962b02e81aebc54cf
maherb <maherb@rpi.edu>
2017-11-11 18:07:43 -0500

    Added filtering restaurants and review extraction

--- itinerary_main.py
+++ itinerary_main.py
@@ -8,13 +8,17 @@ from geopy.geocoders import Nominatim
 
 API_KEY =  "521d44db-20a6-45f9-9f25-836918a17934"
 
-class transportation:
+class TransportType:
     WALKING = 2
     BIKING = 5
     CAR = 10
     #Public transportation is weird because we need to factor in dropoff locations
     #BUS = ???
     #SUBWAY = ???
+
+class LocationType:
+    ATTRACTIONS = 'attractions'
+    RESTAURANTS = 'restaurants'
 
 def main():
     values = driver()
@@ -26,11 +30,11 @@ def main():
         
     #print json.dumps(r.json(), indent=4, sort_keys=True)
 
-def driver():
+def driver(location=LocationType.RESTAURANTS):
     address = input("Please enter an address: ")
     while True:
         try:
-            return getPlacesInArea(address, 9, 18, 100, transportation.WALKING)
+            return getPlacesInArea(address, 9, 18, 100, distance=TransportType.WALKING, location=location)
         except Exception as e:
             print("Address not found. Please enter a different location.")
             print(e)
@@ -38,16 +42,19 @@ def driver():
 
 #End goal is to fill parameters in using forms on the front end
 
-def getPlacesInArea(address, start_time, end_time, price_range, distance=transportation.WALKING):
+def getPlacesInArea(address, start_time, end_time, price_range, location=LocationType.RESTAURANTS, distance=TransportType.WALKING):
 	
     try:
         loc = geolocator(address)
     except Exception as e:
         raise e
-        
-    r = requests.get('https://api.tripadvisor.com/api/partner/2.0/map/{},{}/restaurants?key={}&distance={}'.format(loc['lat'], loc['lng'], API_KEY, distance))
     
-    print(json.dumps(r.json(), indent=4, sort_keys=True))
+    request_url = ('https://api.tripadvisor.com/api/partner/2.0/map/{},{}/{}' \
+            + '?key={}&distance={}').format(loc['lat'], loc['lng'], location, API_KEY, distance)
+    print(request_url)
+    return requests.get(request_url).json()
+    
+    #print(json.dumps(r.json(), indent=4, sort_keys=True))
 
     '''
     #print json.dumps(r.json(), indent=4, sort_keys=True)
@@ -82,12 +89,23 @@ def getPlacesInArea(address, start_time, end_time, price_range, distance=transpo
     return values
     '''
 
+def isRestaurant(attraction):
+    return attraction['category'] == LocationType.RESTAURANTS
+
+def filterRestaurants(dump, restaurants=True):
+    return {'data': [attraction for attraction in dump['data'] \
+            if isRestaurant(attraction) == restaurants]}
+
+def getReviews(attraction):
+    locationId = attraction["location_id"]
+    request_url = 'https://api.tripadvisor.com/api/partner/2.0/location/{0}/reviews?key={1}'\
+        .format(locationId, API_KEY)
+    return requests.get(request_url).json()
+
 def geolocator(address):
     geolocator = Nominatim()
     location = geolocator.geocode(address)
     print(location.address)
-
-    #print location.latitude, location.longitude
 
     return {'lat':location.latitude, 'lng':location.longitude}
 
