0e22ecd8d16a8d912ec848aa4542d5f04da337f6
Danny Wilkins <daniel.wilkins@nyu.edu>
2017-11-11 18:21:08 -0500

    Basic Flask functionality

--- itinerary_main.py
+++ itinerary_main.py
@@ -5,6 +5,10 @@ import operator
 import numpy as np
 from scipy import stats
 from geopy.geocoders import Nominatim
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
 
 API_KEY =  "521d44db-20a6-45f9-9f25-836918a17934"
 
@@ -19,27 +23,33 @@ class transportation:
 def main():
     values = driver()
     
-    print(values)
-    
-    '''for i in values:
-		print i[0], i[1][0], i[1][1]'''
+    #print(values)
+    for i in values:
+        print(i[0], i[1][0], i[1][1])
         
     #print json.dumps(r.json(), indent=4, sort_keys=True)
 
-def driver():
-    address = input("Please enter an address: ")
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/search/<location>')
+def driver(location):
+    #location = input("Please enter an address: ")
+
+    #In the end, if this fails, it would be best to direct to a "failed to find address" page. From there, this can all be called again from the top. No loops or anything.
+
     while True:
         try:
-            return getPlacesInArea(address, 9, 18, 100, transportation.WALKING)
+            return getPlacesInArea(location, 9, 18, 100, transportation.WALKING)
         except Exception as e:
             print("Address not found. Please enter a different location.")
             print(e)
             address = input("Please enter an address: ")
-
 #End goal is to fill parameters in using forms on the front end
 
-def getPlacesInArea(address, start_time, end_time, price_range, distance=transportation.WALKING):
-	
+def getPlacesInArea(address, start_time, end_time, price_range, distance=transportation.WALKING): 
+
     try:
         loc = geolocator(address)
     except Exception as e:
@@ -47,7 +57,7 @@ def getPlacesInArea(address, start_time, end_time, price_range, distance=transpo
         
     r = requests.get('https://api.tripadvisor.com/api/partner/2.0/map/{},{}/restaurants?key={}&distance={}'.format(loc['lat'], loc['lng'], API_KEY, distance))
     
-    print(json.dumps(r.json(), indent=4, sort_keys=True))
+    return #json.dumps(r.json(), indent=4, sort_keys=True)
 
 def geolocator(address):
     geolocator = Nominatim()
